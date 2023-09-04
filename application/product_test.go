d105decd578d37839a8e1d829edf61a176e323c6
Filipe Cavalcante <filipe.cavalcante@grancursosonline.com.br>
2023-09-04 18:11:17 -0300

    arquitetura hexagonal

---
+++ application/product_test.go
@@ -0,0 +1,62 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/filipe/exagonal/application"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Product 1"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Product 1"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero to disable the product", err.Error())
+}
+
+func TestProduct_ISValid(t *testing.T) {
+	product := application.Product{}
+	product.Id = uuid.New().String()
+	product.Name = "Product 1"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater than zero", err.Error())
+}
