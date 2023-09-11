8f655a3424bb3b555ac42a3c4ee9b97253fd1676
Filipe Cavalcante <filipe.cavalcante@grancursosonline.com.br>
2023-09-11 12:40:15 -0300

    db connect

---
+++ adapters/db/product_test.go
@@ -0,0 +1,55 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/filipe/exagonal/adapters/db"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+		"id" string,
+		"name" string,
+		"price" float64,
+		"status" string
+	);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into products values("abc", "Product 1", 0, "disabled");`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func TestProductDB_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product 1", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+
+}
